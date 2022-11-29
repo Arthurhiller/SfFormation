470eafadf11ad3d8756bfd1e756e705eca9d3127
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2022-11-29 11:05:30 +0100

    Affichage de toutes les catégories

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
 {
     /**
      * @Route("/categorie", name="app_categorie")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $categorie = $doctrine->getRepository(Categorie::class)->findBy([], ["intitule" => "DESC"]);
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categories' => $categorie,
         ]);
     }
 }
